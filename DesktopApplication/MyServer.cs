8ae55b4c4fae78ce4bd4a5f2ea7ed65b651710f6
gmike <mmgurgul@gmail.com>
2014-04-15 18:15:42 +0200

    first commit

---
+++ DesktopApplication/MyServer.cs
@@ -0,0 +1,169 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace PosServer
+{
+    public class StateObject
+    {
+        public Socket workSocket = null;
+        public const int BufferSize = 2048;
+        public byte[] buffer = new byte[BufferSize];
+        public StringBuilder sb = new StringBuilder();
+    }
+
+    public class MyServer
+    {
+        static String ip;
+
+        public static String Ip
+        {
+            get { return ip; }
+            set { ip = value; }
+        }
+
+        public static ManualResetEvent allDone = new ManualResetEvent(false);
+
+        public MyServer()
+        {
+        }
+
+        public static void StartListening()
+        {
+            byte[] bytes = new Byte[1024];
+
+            IPHostEntry ipHostInfo = Dns.Resolve(Dns.GetHostName());
+            IPAddress ipAddress = ipHostInfo.AddressList[0];
+            IPEndPoint localEndPoint = new IPEndPoint(ipAddress, 11000);
+            Socket listener = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            ip = ipAddress.ToString();
+            try
+            {
+                listener.Bind(localEndPoint);
+                listener.Listen(100);
+
+                while (true)
+                {
+                    allDone.Reset();
+
+                    Console.WriteLine("Waiting for a connection");
+                    listener.BeginAccept(new AsyncCallback(AcceptCallback), listener);
+
+                    allDone.WaitOne();
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+            Console.WriteLine("\n Press enter to continue");
+            Console.Read();
+        }
+
+        public static void AcceptCallback(IAsyncResult ar)
+        {
+            allDone.Set();
+
+            Socket listener = (Socket)ar.AsyncState;
+            Socket handler = listener.EndAccept(ar);
+
+            StateObject state = new StateObject();
+            state.workSocket = handler;
+            handler.BeginReceive(state.buffer, 0, StateObject.BufferSize, 0, new AsyncCallback(ReadCallback), state);
+        }
+
+        public static void ReadCallback(IAsyncResult ar)
+        {
+            String content = String.Empty;
+
+            StateObject state = (StateObject)ar.AsyncState;
+            Socket handler = state.workSocket;
+
+            int bytesRead = handler.EndReceive(ar);
+
+            if (bytesRead > 0)
+            {
+                state.sb.Append(Encoding.ASCII.GetString(state.buffer, 0, bytesRead));
+                content = state.sb.ToString();
+
+                if (content.IndexOf("<EOF>") > -1)
+                {
+                    
+                    Console.WriteLine("Read {0} bytes from socket. \n Data : {1}", content.Length, content);
+                    Menu menu = new Menu();
+                    String data = konwerter(menu);
+                    Send(handler, data);
+                }
+                else
+                {
+                    handler.BeginReceive(state.buffer, 0, StateObject.BufferSize, 0, new AsyncCallback(ReadCallback), state);
+                }
+            }
+        }
+
+        [JsonObject(MemberSerialization.OptIn)]
+        public class Product
+        {
+            [JsonProperty]
+            public String Nazwa {get;set;}
+
+            [JsonProperty]
+            public float Cena { get; set; }
+        }
+
+        [JsonObject(MemberSerialization.OptIn)]
+        public class Menu
+        {
+            [JsonProperty]
+            LinkedList<Product> menu;
+
+            public Menu()
+            {
+                menu = new LinkedList<Product>();
+                menu.AddLast(new Product(){Nazwa = "wino", Cena = 99.00F});
+                menu.AddLast(new Product() { Nazwa = "kotlet", Cena = 25.50F });
+            }
+        }
+        public static String konwerter(Menu menu)
+        {
+
+            string data = JsonConvert.SerializeObject(menu);
+            return data;
+
+        }
+
+        private static void Send(Socket handler, String data)
+        {
+            byte[] byteData = Encoding.ASCII.GetBytes(data);
+            handler.BeginSend(byteData, 0, byteData.Length, 0, new AsyncCallback(SendCallback), handler);
+        }
+
+        private static void SendCallback(IAsyncResult ar)
+        {
+            try
+            {
+                Socket handler = (Socket)ar.AsyncState;
+
+                int bytesSent = handler.EndSend(ar);
+                Console.WriteLine("Sent {0} bytes to client.", bytesSent);
+
+                handler.Shutdown(SocketShutdown.Both);
+                handler.Close();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+        }
+
+
+
+     
+    }
+}
